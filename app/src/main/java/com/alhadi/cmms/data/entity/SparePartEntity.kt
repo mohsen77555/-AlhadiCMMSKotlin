c870e18a0d67a693342a3786f8a4348885321ec8
mohsen77555 <65857893+mohsen77555@users.noreply.github.com>
2026-06-19 13:32:03 +0300

    feat(stage22-23): extend spare-part governance

--- app/src/main/java/com/alhadi/cmms/data/entity/SparePartEntity.kt
+++ app/src/main/java/com/alhadi/cmms/data/entity/SparePartEntity.kt
@@ -1,10 +1,10 @@
 package com.alhadi.cmms.data.entity
 
-import kotlinx.serialization.Serializable
-
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
 @Entity(
     tableName = "spare_parts",
@@ -21,5 +21,19 @@ data class SparePartEntity(
     val onHandQty: Int,
     val minQty: Int,
     val location: String,
-    val lastPrice: Double
+    val lastPrice: Double,
+    @ColumnInfo(defaultValue = "0")
+    val isSerialized: Boolean = false,
+    @ColumnInfo(defaultValue = "0")
+    val requiresBatch: Boolean = false,
+    @ColumnInfo(defaultValue = "''")
+    val manufacturerPartNumber: String = "",
+    @ColumnInfo(defaultValue = "''")
+    val valuationClass: String = "",
+    @ColumnInfo(defaultValue = "'SAR'")
+    val currency: String = "SAR",
+    @ColumnInfo(defaultValue = "0.0")
+    val standardPrice: Double = 0.0,
+    @ColumnInfo(defaultValue = "''")
+    val lastCountedAt: String = ""
 )
